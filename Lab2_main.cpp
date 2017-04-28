258c66a5ef3a9aa220a17beca45008c7f5546b32
marvinestrada21 <marvinestrada2@gmail.com>
2017-04-28 15:49:05 -0600

    commit 2

--- Lab2_main.cpp
+++ Lab2_main.cpp
@@ -22,17 +22,67 @@ int main(){
                 break;}
 
             case 2:{
-                int num1, num2, num3, num4;
-		cout << "Ingrese la primera coordenada: ";
-		cin >> num1;
-		cout << "Ingrese la segunda coordenada: ";
-		cin >> num2;
-		cout << "Ingrese la tercera coordenada: ";
-		cin >> num3;
-		cout << "Ingrese la cuarta coordenada: ";
-		cin >> num4;
+                int x1, y1, x2, y2, x3, y3, x4, y4;
+		double d1, d2, d3, d4;
+		double dt1, dt2, dt3;
+		double dtt1, dtt2, dtt3;
+		cout << "Ingrese x de la primera coordenada: ";
+		cin >> x1;
+		cout << "Ingrese y de la primera coordenada: ";
+		cin >> y1;
+		cout << "Ingrese x de la segunda coordenada: ";
+		cin >> x2;
+		cout << "Ingrese y de la segunda coordenada: ";
+		cin >> y2;
+		cout << "Ingrese x de la tercera coordenada: ";
+		cin >> x3;
+		cout << "Ingrese y de la tercera coordenada: ";
+		cin >> y3;
+		cout << "Ingrese x de la cuarta coordenada: ";
+		cin >> x4;
+		cout << "Ingrese y de la cuarta coordenada: ";
+		cin >> y4;
+		//Distancias de Trapezoide
+		d1 = sqrt((pow((x2 - x1), 2)) + (pow((y2 - y1), 2)));
+		d2 = sqrt((pow((x3 - x2), 2)) + (pow((y3 - y2), 2)));
+		d3 = sqrt((pow((x4 - x3), 2)) + (pow((y4 - y3), 2)));
+		d4 = sqrt((pow((x1 - x4), 2)) + (pow((y1 - y4), 2)));
+		//Distancias de Triangulo 1
+		dt1 = sqrt((pow((x2 - x1), 2)) + (pow((y2 - y1), 2)));
+		dt2 = sqrt((pow((x4 - x2), 2)) + (pow((y4 - y2), 2)));
+		dt3 = sqrt((pow((x1 - x4), 2)) + (pow((y1 - y4), 2)));
+		//Distancias de Triangulo 2
+		dtt1 = sqrt((pow((x2 - x3), 2)) + (pow((y2 - y3), 2)));
+		dtt2 = sqrt((pow((x3 - x4), 2)) + (pow((y3 - y2), 2)));
+		dtt3 = sqrt((pow((x2 - x4), 2)) + (pow((y2 - y4), 2)));
 
-                break;}
+		cout << "Los puntos son: " << endl;
+		cout << "(" << x1 << ", " << y1 << ")" << endl;
+		cout << "(" << x2 << ", " << y2 << ")" << endl;
+		cout << "(" << x3 << ", " << y3 << ")" << endl;
+		cout << "(" << x4 << ", " << y4 << ")" << endl;
+		cout << " " << endl;		
+
+		cout << "Los lados del trapezoide miden: " << endl;
+		cout << "Lado 1: " << d1 << endl;
+		cout << "Lado 2: " << d2 << endl;
+		cout << "Lado 3: " << d3 << endl;
+		cout << "Lado 4: " << d4 << endl;
+		cout << " " << endl;
+
+		cout << "Los lados del triangulo 1 miden: " << endl;
+		cout << "Lado 1: " << dt1 << endl;
+		cout << "Lado 2: " << dt2 << endl;
+		cout << "Lado 3: " << dt3 << endl;
+		cout << " " << endl;
+
+		cout << "Los lados del triangulo 2 miden: " << endl;
+		cout << "Lado 1: " << dtt1 << endl;
+		cout << "Lado 2: " << dtt2 << endl;
+		cout << "Lado 3: " << dtt3 << endl;
+		cout << " " <<  endl;
+
+		break;}
 
             case 3:
                 salir = true;
